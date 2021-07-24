4d223750028f529c19eca411f5d1f6121f590041
Joao Guimaraes <dezborges@gmail.com>
2021-07-24 14:56:33 -0300

    vai

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,7 +7,7 @@ function Login() {
     <div>
       <div className="welcome">
         <div className="Img">
-          <h1 className="text1">Faça Login para receber descontos! </h1>
+          <h1 className="text1">Cadastre produtos e clientes aqui ! </h1>
           <img src={Casd} className="Casd" alt=""></img>
         </div>
         <div className="cadastroContainer">
